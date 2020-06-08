6e425e9277a0b48a278a42eaf401f7e1f362f59c
Brendan Le Glaunec <brendan.le-glaunec@epitech.eu>
2020-06-08 12:18:05 +0200

    Make cache status header opt-out

--- pkg/config/dynamic/traefikee_middlewares.go
+++ pkg/config/dynamic/traefikee_middlewares.go
@@ -234,16 +234,17 @@ type OIDCAuthSession struct {
 
 // HTTPCache holds the HTTP Cache Middleware configuration.
 type HTTPCache struct {
-	MaxTTL               int      `json:"maxTtl,omitempty" toml:"maxTtl,omitempty" yaml:"maxTtl,omitempty"`
-	ResponseCode         []int    `json:"responseCode,omitempty" toml:"responseCode,omitempty" yaml:"responseCode,omitempty"`
-	RequestMethod        []string `json:"requestMethod,omitempty" toml:"requestMethod,omitempty" yaml:"requestMethod,omitempty"`
-	DisableCacheControl  bool     `json:"disableCacheControl,omitempty" toml:"disableCacheControl,omitempty" yaml:"disableCacheControl,omitempty"`
-	AddCacheStatusHeader bool     `json:"addCacheStatusHeader,omitempty" toml:"addCacheStatusHeader,omitempty" yaml:"addCacheStatusHeader,omitempty"`
-	MemoryLimit          string   `json:"memoryLimit,omitempty" toml:"memoryLimit,omitempty" yaml:"memoryLimit,omitempty"`
+	MaxTTL                   int      `json:"maxTtl,omitempty" toml:"maxTtl,omitempty" yaml:"maxTtl,omitempty"`
+	ResponseCode             []int    `json:"responseCode,omitempty" toml:"responseCode,omitempty" yaml:"responseCode,omitempty"`
+	RequestMethod            []string `json:"requestMethod,omitempty" toml:"requestMethod,omitempty" yaml:"requestMethod,omitempty"`
+	DisableCacheControl      bool     `json:"disableCacheControl,omitempty" toml:"disableCacheControl,omitempty" yaml:"disableCacheControl,omitempty"`
+	DisableCacheStatusHeader bool     `json:"disableCacheStatusHeader,omitempty" toml:"disableCacheStatusHeader,omitempty" yaml:"disableCacheStatusHeader,omitempty"`
+	MemoryLimit              string   `json:"memoryLimit,omitempty" toml:"memoryLimit,omitempty" yaml:"memoryLimit,omitempty"`
 }
 
 // SetDefaults sets default values for an HTTP cache middleware.
 func (h *HTTPCache) SetDefaults() {
 	h.MaxTTL = 300
 	h.RequestMethod = []string{"GET", "HEAD"}
+	h.MemoryLimit = "1Gi"
 }
