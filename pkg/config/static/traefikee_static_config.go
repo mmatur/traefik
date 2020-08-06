8efcc5cc851b940b7c54b31aab99ec67936fd5a8
LandryBe <landry@containo.us>
2020-08-06 14:20:04 +0200

    Use duration instead of seconds to define interval for Vault provider

--- pkg/config/static/traefikee_static_config.go
+++ pkg/config/static/traefikee_static_config.go
@@ -1,20 +1,26 @@
 package static
 
+import (
+	"time"
+
+	"github.com/containous/traefik/v2/pkg/types"
+)
+
 // Vault configures the Vault provider for TLS certificates.
 type Vault struct {
 	URL   string `description:"URL of the Vault API" json:"url" toml:"url" yaml:"url" export:"true"`
 	Token string `description:"Token used to authenticate with the API" json:"token" toml:"token" yaml:"token" export:"true"`
 	Path  string `description:"Path where TLS certificates are located" json:"path" toml:"path" yaml:"path" export:"true"`
 
-	SyncInterval   int `description:"Interval in seconds to synchronize new and deleted certificates" json:"syncInterval" toml:"syncInterval" yaml:"syncInterval" export:"true"`
-	RescanInterval int `description:"Interval in seconds to rescan all certificates for changes" json:"rescanInterval" toml:"rescanInterval" yaml:"rescanInterval" export:"true"`
+	SyncInterval   types.Duration `description:"Interval to synchronize new and deleted certificates" json:"syncInterval" toml:"syncInterval" yaml:"syncInterval" export:"true"`
+	RescanInterval types.Duration `description:"Interval to rescan all certificates for changes" json:"rescanInterval" toml:"rescanInterval" yaml:"rescanInterval" export:"true"`
 }
 
 // SetDefaults sets the default values on the Vault provider configuration.
 func (p *Vault) SetDefaults() {
 	p.Path = "tls"
-	p.SyncInterval = 5
-	p.RescanInterval = 60
+	p.SyncInterval = types.Duration(5 * time.Second)
+	p.RescanInterval = types.Duration(60 * time.Second)
 }
 
 // VaultPKI configures Vault as a certificate resolver.
